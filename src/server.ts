01fd1aa340eb81cf0eb232560a11486cba43b8b9
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-07-18 02:25:32 +0600

    feat: set up Express server with TypeScript and nodemon

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import config from '@/config';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, World!'
+    });
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on http://localhost:${config.PORT}`);
+});
