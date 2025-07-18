ada5355fa8a7de61daae105adb6ee79f2a3457a6
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-07-18 15:51:20 +0600

    feat: added CORS support and update package dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT || 3000,
+    NODE_ENV: process.env.NODE_ENV,
+    WHITELIST_ORIGINS: [
+        'http://blog-api-nodejs-ts.onrender.com',
+    ],
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import express from 'express';
+import cors from 'cors';
+
 import config from '@/config';
 
+import type { CorsOptions } from 'cors';
+
 const app = express();
+
+// configure CORS options
+const corsOptions: CorsOptions = {
+    origin(origin, callback) {
+        if (config.NODE_ENV === 'development' || !origin || config.WHITELIST_ORIGINS.includes(origin)) {
+            callback(null, true);
+        } else {
+            // reject request from non-whitelisted origins
+            callback(new Error(`CORS Error: ${origin} is not allowed`), false);
+            console.log(`CORS Error: ${origin} is not allowed`);
+        }
+    },
+};
+
+
+// apply cors middleware
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.json({
